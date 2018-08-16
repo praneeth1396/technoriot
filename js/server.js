a92e48983aa6b80421ee37db5625749cd7aac18e
Praneeth <npraneeth.arsenal@gmail.com>
2018-08-16 21:39:47 +0530

    Final

--- js/server.js
+++ js/server.js
@@ -10,8 +10,36 @@ app.use(function(req, res, next) {
     next();
 });
 
+const { Client } = require('pg');
+
+
 app.post('/send_params', function(req, res) {
-    console.log(req.body.finalResult);
+    console.log(req.body.finalResult);    
+    const client = new Client({
+        connectionString: process.env.DATABASE_URL,
+        ssl: true,
+    });
+    client.connect();
+    client.query("SELECT user_id from users;", (err, res) => {
+        if (err) console.log(err);
+        for (let row of res.rows) {
+          console.log(JSON.stringify(row));
+        }
+        client.end();
+    });
+    var users = req.body.finalResult;
+    var index = 0;
+    var req_fields = {gender:3,college_code:6,college_name:7,name:8,phone_number:9,email:10};
+    for(index;index<results.length;index++){
+        var user = results[index];
+        var full_name = user[name].Value;
+        var email = user[email].Value;
+        var phone_number = user[phone_number].Value;
+        var gender = user[gender].Value;
+        var college_name = user[college_name].Value;
+        var college_code = user[college_code].Value;
+        console.log(full_name + " " + email + " " + email + " " + phone_number + " " + gender + " " + college_name + " " + college_code);
+    }
     //check credentials with db here
 });
 
