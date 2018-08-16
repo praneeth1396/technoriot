ba4bf9c0e10cc3397fb0edaed497efb1c7fee44c
Praneeth <npraneeth.arsenal@gmail.com>
2018-08-16 22:49:10 +0530

    Final

--- js/server.js
+++ js/server.js
@@ -17,7 +17,7 @@ const { Client } = require('pg');
 var port = process.env.PORT || 8080;
 
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname + "/../index.php"));
+    res.render(path.join(__dirname + "/../index.php"));
 });
 
 app.post('/send_params', function(req, res) {
